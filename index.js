2042f54fcd27a8e5db03a3684ed2cd32007eb372
Aswini-mca <aswini25.mca@gmail.com>
2024-01-31 12:27:10 +0530

    updated

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { MongoClient } from "mongodb";
 import cors from "cors";
 import { usersRouter } from './routes/user.js';
+import 'dotenv/config';
 
 const app = express();
 const PORT = 9000;
